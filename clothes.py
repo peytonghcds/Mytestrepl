075ab6b2bc3c1c7a7b419cbe7402d25cab5f84b9
peytonghcds <102323269+peytonghcds@users.noreply.github.com>
2022-04-06 19:16:06 +0000

    clothes

---
+++ clothes.py
@@ -0,0 +1,25 @@
+class pants (self, waist,length,color):
+  def __init__ (self,waist,length,color)
+    self.waist = waist
+    self.length = length
+    self.color = color 
+
+
+class shirt:
+  def __init__(self,color,numButtons,isshortsleeve,hasCollar):
+    self.color = color
+    self.numbuttons = numButtons
+    self.isshortsleeve = isshortsleeve
+    self.hascollar = hascollar
+
+
+
+
+
+
+
+
+
+
+
+
